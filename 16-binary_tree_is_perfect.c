cc8158916391bb5b13549a01c8043b2fa80c6487
mathewkoech <mathewkoech55@gmail.com>
2024-03-26 07:24:00 -0700

    function that measures the size of a binary tree

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_tree.h
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_perfect - Check if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree
+ * Return: true if the tree is perfect, otherwise false
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	
+}
